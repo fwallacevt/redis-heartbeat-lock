ae563a35699b721f1795dd18e1ec74c1c95a9b92
Forrest Wallace <forrest.wallace.vt@gmail.com>
2020-07-08 11:59:58 -0400

    Fixing async redis lock

--- redis_heartbeat_lock/async_redis_lock.py
+++ redis_heartbeat_lock/async_redis_lock.py
@@ -26,20 +26,30 @@ class AsyncRedisLock:
     # Expiration of the lock, in seconds
     lock_expiry: int
 
-    def __init__(self, key: str, client: redis.Redis):
+    def __init__(
+        self,
+        key: str,
+        client: redis.Redis,
+        lock_acquisition_timeout: float,
+        lock_check_rate: float,
+        lock_expiry: int,
+    ):
         self.key = key
         self.client = client
+        self.lock_acquisition_timeout = lock_acquisition_timeout
+        self.lock_check_rate = lock_check_rate
+        self.lock_expiry = lock_expiry
 
     @classmethod
     async def create(
         cls,
         key: str,
-        host: str,
+        host: str = "127.0.0.1",
         port: int = 6379,
         db: int = 0,
         lock_acquisition_timeout: float = 8.0,
         lock_check_rate: float = 0.2,
-        lock_expiry: float = 8.0,
+        lock_expiry: int = 8,
     ) -> "AsyncRedisLock":
         """Asynchronously create a Redis client and initialize the wrapper class."""
 
@@ -47,22 +57,22 @@ class AsyncRedisLock:
             return redis.Redis(host, port, db)
 
         client = await run_sync_in_thread_pool(_inner)
-        return cls(key, client)
+        return cls(key, client, lock_acquisition_timeout, lock_check_rate, lock_expiry)
 
     async def set_lock(self, value: Any, nx: bool = False) -> None:
         """Try to set the given key until we timeout."""
 
         def _inner():
-            while (
-                self.client.set(name=self.key, value=value, ex=self.lock_expiry, nx=nx)
-                is not True
+            _start_time = time.time()
+            while self.client.set(
+                name=self.key, value=str(value), ex=self.lock_expiry, px=None, nx=nx,
+            ) is not True and (
+                (time.time() - _start_time) < self.lock_acquisition_timeout
             ):
                 time.sleep(self.lock_check_rate)
 
         # Timeout after our specified number of seconds
-        await asyncio.wait_for(
-            run_sync_in_thread_pool(_inner), timeout=self.lock_acquisition_timeout,
-        )
+        await run_sync_in_thread_pool(_inner)
 
     async def expire(self) -> None:
         """Set an expire flag on the given key."""
